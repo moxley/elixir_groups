6a0dae3f1455ce09da37a7d10cd842f8ed8c8301
dingo sky <paul@dingosky.com>
2018-02-24 10:45:39 -0800

    Boilerplate

---
+++ elixir_groups/lib/elixir_groups.ex
@@ -0,0 +1,18 @@
+defmodule ElixirGroups do
+  @moduledoc """
+  Documentation for ElixirGroups.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirGroups.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir_groups/test/elixir_groups_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirGroupsTest do
+  use ExUnit.Case
+  doctest ElixirGroups
+
+  test "greets the world" do
+    assert ElixirGroups.hello() == :world
+  end
+end
