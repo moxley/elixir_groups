162dc4a66723aa5af4158c84e6ab9968cb975e23
Dan Porter <dan.porter@he3labs.com>
2018-03-17 12:31:18 -0700

    Patch PageControllerTest

--- test/elixir_groups_web/controllers/page_controller_test.exs
+++ test/elixir_groups_web/controllers/page_controller_test.exs
@@ -2,7 +2,7 @@ defmodule ElixirGroupsWeb.PageControllerTest do
   use ElixirGroupsWeb.ConnCase
 
   test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    conn = get(conn, "/")
+    assert html_response(conn, 200) =~ "Welcome to PDX Elixir"
   end
 end
